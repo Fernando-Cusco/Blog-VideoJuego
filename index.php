1efa85ecf6bfb2767de17c8e58710e806c210274
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-12 19:49:25 -0500

    [add] navbar and menu
    creamos la barra de navegación y el menú.

--- index.php
+++ index.php
@@ -13,8 +13,36 @@
           Blog de Juegos
         </a>
       </div>
+      <!--Menu-->
+      <nav id="nav">
+        <ul>
+          <li>
+            <a href="index.php">Inicio</a>
+          </li>
+          <li>
+            <a href="index.php">Accion</a>
+          </li>
+          <li>
+            <a href="index.php">Arcade</a>
+          </li>
+          <li>
+            <a href="index.php">Deportes</a>
+          </li>
+          <li>
+            <a href="index.php">Horror</a>
+          </li>
+          <li>
+            <a href="index.php">Recomendacion</a>
+          </li>
+          <li>
+            <a href="index.php">Acerca de</a>
+          </li>
+          <li>
+            <a href="index.php">Contactanos</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-    <!--Menu-->
     <!--Sidebar-->
     <!--Contenido-->
     <!--Pie de pagina-->

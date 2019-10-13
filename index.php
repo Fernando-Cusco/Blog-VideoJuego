6e969f36e2488e5d79bbdc49627e1949938add07
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-12 19:36:51 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Mi Blog VideoJuegos</title>
+  </head>
+  <body>
+
+    <!--Cabecera-->
+    <!--Menu-->
+    <!--Sidebar-->
+    <!--Contenido-->
+    <!--Pie de pagina-->
+
+  </body>
+</html>

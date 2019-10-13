da5d153bc9232c38b09b46ab5e5b918b04989a59
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-12 19:43:06 -0500

    [add] create header
    creamos la cabecera

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
   <body>
 
     <!--Cabecera-->
+    <header>
+      <div id="logo">
+        <a href="index.php">
+          Blog de Juegos
+        </a>
+      </div>
+    </header>
     <!--Menu-->
     <!--Sidebar-->
     <!--Contenido-->

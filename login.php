df61b5d3598d97c7061809f04ef27f9e012b0583
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-16 22:34:20 -0500

    [add] login
    Agregamos el login de usuario.

--- login.php
+++ login.php
@@ -1,7 +1,33 @@
 <?php
   //iniciar sesion y la conexion a la db.
-  
+  require_once 'includes/conexion.php';
   //obtener trader_cdlrisefall3methods
-  //verificar los trader_cdlrisefall3methods
-  //utilizar una sesion para guardar los datos del usuario logeado
+  if(isset($_POST)){
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    //comprobar credeciales del usuario
+    $sql = "select * from usuarios where email = '$email'";
+    $login = mysqli_query($db, $sql);
+    if($login && mysqli_num_rows($login) == 1){
+      $usuario = mysqli_fetch_assoc($login);
+      //verificar la password    me llega de la base de datos con el mysqli_fetch_assoc
+      $verificar = password_verify($password, $usuario['password']);
+      if($verificar){
+        //utilizar una sesion para guardar los datos del usuario logeado
+        $_SESSION['usuario'] = $usuario;
+        //borramos la sesion del error en el caso de que la primera vez no dio el error al iniciar sesion
+        if(isset($_SESSION['error_login'])){
+          session_unset($_SESSION['error_login']);
+        }
+      } else {
+        //enviar al usuario el fallo
+        $_SESSION['error_login'] = "Password Incorrecta";
+      }
+    } else {
+      $_SESSION['error_login'] = "Email Incorrecto";
+    }
+  }
+  header('Location: index.php');
+
 ?>
--- registro.php
+++ registro.php
@@ -6,9 +6,9 @@
     }
     //usamos operadores ternarios evitar varios if's
     //        si existe                true                 false
-    $nombre = isset($_POST['nombre']) ? mysqli_real_escape_string($db, $_POST['nombre']) : false;
-    $apellidos = isset($_POST['apellidos']) ? mysqli_real_escape_string($db, $_POST['apellidos']) : false;
-    $email = isset($_POST['email']) ? mysqli_real_escape_string($db, $_POST['email']) : false;
+    $nombre = isset($_POST['nombre']) ? mysqli_real_escape_string($db, trim($_POST['nombre'])) : false;
+    $apellidos = isset($_POST['apellidos']) ? mysqli_real_escape_string($db, trim($_POST['apellidos'])) : false;
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($db, trim($_POST['email'])) : false;
     $password = isset($_POST['password']) ? mysqli_real_escape_string($db, $_POST['password']) : false;
 
     //arreglo de errores

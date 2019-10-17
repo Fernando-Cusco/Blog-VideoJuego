def7b1a95e4d611902831c89a02d2b7be830a3c7
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-16 23:15:41 -0500

    [add] Add Buttons
    Agregamos botones para crear un post, editar el perfil y cerrar sesion.

--- login.php
+++ login.php
@@ -3,6 +3,10 @@
   require_once 'includes/conexion.php';
   //obtener trader_cdlrisefall3methods
   if(isset($_POST)){
+    //borramos la sesion del error en el caso de que la primera vez no dio el error al iniciar sesion
+    if(isset($_SESSION['error_login'])){
+      session_unset();
+    }
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
 
@@ -16,16 +20,12 @@
       if($verificar){
         //utilizar una sesion para guardar los datos del usuario logeado
         $_SESSION['usuario'] = $usuario;
-        //borramos la sesion del error en el caso de que la primera vez no dio el error al iniciar sesion
-        if(isset($_SESSION['error_login'])){
-          session_unset($_SESSION['error_login']);
-        }
       } else {
         //enviar al usuario el fallo
         $_SESSION['error_login'] = "Password Incorrecta";
       }
     } else {
-      $_SESSION['error_login'] = "Email Incorrecto";
+      $_SESSION['error_login'] = "Login Incorrecto";
     }
   }
   header('Location: index.php');
